1bb0392448e3ddd7204f15912d4c349ca5e722da
Q <vpinyokool@me.com>
2023-10-01 17:14:02 -0700

    more refinement and copy updates

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,18 +2,33 @@ var $body = $("body");
 var $pin = $("div.fixed");
 var $bf = $(".twentytwenty-container");
 
+function whichDay() {
+	  // Get the current date
+  var currentDate = new Date();
+
+  // Define an array of day names
+  var daysOfWeek = [
+  	"Sunday! Hope you are enjoying your weekend Ben!",
+  	"Monday! Hope you had a nice weekend Ben!",
+  	"Tuesday! Hope you had a nice weekend Ben!",
+  	"Hump day! It's midweek!",
+  	"Thursday!",
+  	"Friday! It's almost the weekend!",
+  	"Saturday! Hope you are enjoying your weekend Ben!"
+  	];
+
+  // Get the day of the week (0 = Sunday, 1 = Monday, ..., 6 = Saturday)
+  var dayIndex = currentDate.getDay();
+
+  // Get the day name from the array
+  var dayName = daysOfWeek[dayIndex];
+
+  // Replace the text of elements with class 'day'
+  $(".day").text(dayName);
+}
 function animateSections() {
 	$("section").each(function (index) {
 		const section = $(this);
-
-		// Set initial properties
-		$body.addClass("_no-scroll");
-		gsap.set(section, {
-			opacity: 0,
-			y: 100,
-			rotationX: 30,
-			transformStyle: "preserve-3d",
-		});
 
 		// Animate using GSAP
 		gsap.to(section, {
@@ -21,11 +36,10 @@ function animateSections() {
 			y: 0,
 			rotationX: 0,
 			duration: 0.8, // Animation duration
-			// duration: 8, // Animation duration
 			delay: 0.5 * index, // Staggered delay
 			ease: "back.out(1.7)",
 			onComplete: function () {
-				console.log("Animation complete");
+				// console.log("Animation complete");
 				$body.removeClass();
 			},
 		});
@@ -37,17 +51,6 @@ function runSwiper() {
       grabCursor: true,
       centeredSlides: true,
       // loop: true,
-
-		// // If we need pagination
-		// pagination: {
-		// 	el: ".swiper-pagination",
-		// },
-
-		// Navigation arrows
-		// navigation: {
-		// 	nextEl: ".swiper-button-next",
-		// 	prevEl: ".swiper-button-prev",
-		// },
 
 		// And if we need scrollbar
 		scrollbar: {
@@ -102,19 +105,33 @@ function animateHeader() {
 			});
 		});
 	}, 1000); // Adjust the delay time as needed
+
 }
 
-function animateLastCharacter(char) {
-	gsap.to(char, {
-		rotate: 24,
-		duration: 0.8,
-		ease: "power1.inOut",
-		delay: 1,
-		repeat: -1, // Infinite loop
-		repeatDelay: 0.1, // Delay before each repeat (in seconds)
-		yoyo: true,
-	});
+	function animateLastCharacter(char) {
+
+	// nothing to see here
+
+	// $(char).addClass('_victory');
+	// 	gsap.to(char, {
+	// 	rotate: 24,
+	// 	y: -200,
+	// 	duration: .4,
+	// 	ease: "power1.inOut",
+	// 	delay: 1,
+	// 	repeat: -1, // Infinite loop
+	// 	repeatDelay: 0.1, // Delay before each repeat (in seconds)
+	// 	yoyo: true,
+	// });
 }
+
+
+
+
+// INIT
+
+$body.addClass("_no-scroll");
+whichDay();
 
 window.onload = function () {
 	var shadowRoot = document.querySelector("spline-viewer").shadowRoot;
